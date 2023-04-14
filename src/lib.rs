145b77d43d09e074dce2b0a1dc830266b055c2c1
Ji Chen <jaykchen@gmail.com>
2023-04-13 20:09:51 -0400

    image with text ocr

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use cloud_vision_flows::text_detection;
 use lambda_flows::{request_received, send_response};
-use tg_flows::{ChatId, InputFile, Telegram, listen_to_update, UpdateKind};
+use tg_flows::{listen_to_update, ChatId, InputFile, Telegram, UpdateKind};
 use url::Url;
 
 #[no_mangle]
@@ -8,39 +8,35 @@ pub fn run() {
     let telegram_token = std::env::var("telegram_token").unwrap();
     let tele = Telegram::new(telegram_token.clone());
 
-    let crustaceans = "https://rustacean.net/assets/rustacean-orig-noshadow.png";
+    let crustaceans = "https://images.template.net/wp-content/uploads/2016/03/02064535/informal-lab-report-template.jpg";
     let url = Url::try_from(crustaceans).unwrap();
-//rebuild
+    //rebuild
     listen_to_update(telegram_token, |update| {
         _ = tele.send_photo(ChatId(6221995180), InputFile::url(url));
+
+        let text = text_detection(String::from_utf8(crustaceans.as_bytes().to_vec()).unwrap());
+        match text {
+            Ok(r) => send_response(
+                200,
+                vec![(
+                    String::from("content-type"),
+                    String::from("text/plain; charset=UTF-8"),
+                )],
+                r.as_bytes().to_vec(),
+            ),
+            Err(e) => send_response(
+                500,
+                vec![(
+                    String::from("content-type"),
+                    String::from("text/plain; charset=UTF-8"),
+                )],
+                e.as_bytes().to_vec(),
+            ),
+        }
 
         if let UpdateKind::Message(msg) = update.kind {
             let mut text = msg.text().unwrap_or("");
             let chat_id = msg.chat.id;
         }
-
-
     });
-
-    // request_received(|_qry, body| {
-    //     let text = text_detection(String::from_utf8(body).unwrap());
-    //     match text {
-    //         Ok(r) => send_response(
-    //             200,
-    //             vec![(
-    //                 String::from("content-type"),
-    //                 String::from("text/plain; charset=UTF-8"),
-    //             )],
-    //             r.as_bytes().to_vec(),
-    //         ),
-    //         Err(e) => send_response(
-    //             500,
-    //             vec![(
-    //                 String::from("content-type"),
-    //                 String::from("text/plain; charset=UTF-8"),
-    //             )],
-    //             e.as_bytes().to_vec(),
-    //         ),
-    //     }
-    // });
 }
