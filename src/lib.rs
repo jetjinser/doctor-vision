503ade314d66afcd86802a8121f05a8c4bf6cd8e
Ji Chen <jaykchen@gmail.com>
2023-04-17 15:30:54 -0400

    upload pic as file download on server sync

--- src/lib.rs
+++ src/lib.rs
@@ -5,68 +5,61 @@ use http_req::{
     response::{Headers, Response},
     uri::Uri,
 };
-use image::GenericImageView;
 use image::{self, Pixel};
 use lambda_flows::{request_received, send_response};
 use serde_json::{json, Value};
+use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Cursor;
 use tg_flows::{listen_to_update, ChatId, InputFile, Telegram, UpdateKind};
-use url::Url;
 
 #[no_mangle]
 pub fn run() {
     let telegram_token = std::env::var("telegram_token").unwrap();
     let tele = Telegram::new(telegram_token.clone());
+    // let file_path = "~/Downloads/lab_report_part.jpg"; // Replace with the path to your image file
+
+    // send_photo_as_document(&bot, chat_id, file_path).await;
 
     listen_to_update(&telegram_token, |update| {
         if let UpdateKind::Message(msg) = update.kind {
-            let image_file_id = msg.photo().unwrap()[0].file.id.clone();
+            let image_file_id = msg.document().unwrap().file.id.clone();
+            // let image_file_id = msg.photo().unwrap()[0].file.id.clone();
             let chat_id = msg.chat.id;
 
-            let photo_data = match download_photo_data(&telegram_token, &image_file_id) {
-                Ok(data) => data,
-                Err(e) => {
-                    eprintln!("Error downloading photo: {}", e);
+            match download_photo_data_base64(&telegram_token, &image_file_id) {
+                Ok(data) => {
+                    let text = text_detection(data);
+
+                    match text {
+                        Ok(r) => {
+                            tele.send_message(chat_id, r.clone());
+
+                            send_response(
+                                200,
+                                vec![(
+                                    String::from("content-type"),
+                                    String::from("text/plain; charset=UTF-8"),
+                                )],
+                                r.as_bytes().to_vec(),
+                            );
+                        }
+                        Err(e) => send_response(
+                            500,
+                            vec![(
+                                String::from("content-type"),
+                                String::from("text/plain; charset=UTF-8"),
+                            )],
+                            e.as_bytes().to_vec(),
+                        ),
+                    }
+                }
+               Err(e)  => {
+                    eprintln!("Error downloading photo");
                     return;
                 }
             };
-
-            let img = image::load_from_memory(&photo_data).unwrap();
-
-            let mut image_buffer = Cursor::new(Vec::new());
-            img.write_to(&mut image_buffer, image::ImageOutputFormat::Png)
-                .expect("Error converting image to byte buffer");
-
-            let image_bytes = image_buffer.into_inner();
-
-            let image_base64 = base64::encode(&image_bytes);
-
-            let text = text_detection(image_base64);
-
-            match text {
-                Ok(r) => {
-                    tele.send_message(chat_id, r.clone());
-
-                    send_response(
-                        200,
-                        vec![(
-                            String::from("content-type"),
-                            String::from("text/plain; charset=UTF-8"),
-                        )],
-                        r.as_bytes().to_vec(),
-                    );
-                }
-                Err(e) => send_response(
-                    500,
-                    vec![(
-                        String::from("content-type"),
-                        String::from("text/plain; charset=UTF-8"),
-                    )],
-                    e.as_bytes().to_vec(),
-                ),
-            }
 
             // if let Some(c) = c {
             //     if c.restarted {
@@ -79,7 +72,23 @@ pub fn run() {
     });
 }
 
-fn download_photo_data(token: &str, file_id: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+// async fn send_photo_as_document(bot: &Bot, chat_id: i64, file_path: &str) {
+//     let input_file = teloxide::types::InputFile::File {
+//         media: teloxide::types::MediaFile::from_file(file_path),
+//         file_name: Some(String::from(file_path)),
+//         thumb: None,
+//     };
+
+//     let document = teloxide::types::InputMediaDocument::new(input_file);
+
+//     if let Err(e) = bot.send_document(chat_id, document).send().await {
+//         eprintln!("Error while sending the document: {}", e);
+//     }
+// }
+pub fn download_photo_data_base64(
+    token: &str,
+    file_id: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
     let file_url = format!(
         "https://api.telegram.org/bot{}/getFile?file_id={}",
         token, file_id
@@ -91,16 +100,7 @@ fn download_photo_data(token: &str, file_id: &str) -> Result<Vec<u8>, Box<dyn st
         .method(Method::GET)
         .send(&mut file_response)?;
 
-    let response: serde_json::Value = serde_json::from_slice(&file_response)?;
+    let base64_encoded = base64::encode(file_response);
 
-    let file_path = response["result"]["file_path"].as_str().unwrap();
-    let photo_url = format!("https://api.telegram.org/file/bot{}/{}", token, file_path);
-    let photo_uri: Uri = Uri::try_from(photo_url.as_str()).unwrap();
-
-    let mut photo_data = Vec::new();
-    Request::new(&photo_uri)
-        .method(Method::GET)
-        .send(&mut photo_data)?;
-
-    Ok(photo_data)
+    Ok(base64_encoded)
 }
