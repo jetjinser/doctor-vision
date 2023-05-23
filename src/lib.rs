64b56095a0effee25476892c990c1e0c130342c7
jinser <cmdr.jv@gmail.com>
2023-05-23 15:12:57 +0800

    echo

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,35 @@
 use std::env;
 
-use state::State;
 use tg_flows::{listen_to_update, Message, Telegram, Update, UpdateKind};
 
-mod chat;
-mod doctor;
-mod state;
-mod telegram;
+#[no_mangle]
+#[tokio::main]
+pub async fn run() {
+    let telegram_token = env::var("telegram_token").unwrap();
 
-const HELP: &str = "Hello! I am your medical lab report analyzer bot. Zoom in on where you need assistance with, take a photo and upload it as a file, or paste the photo in the chatbox to send me if you think it's clear enough.";
-
-const SYSTEM: &str = r#"You are a medical lab technican, you'll read a lab report and tell the user the most important findings of the report in short bullets, please use the following template: The major findings are:
-                        1) [the name of the measurement] [status of the reading]
-                        ...
-                        one sentence summary about the subject's health status."#;
-
-struct App {
-    tele: Telegram,
-    tele_token: String,
-    openai_key: String,
-    msg: Message,
+    listen_to_update(telegram_token.clone(), |update| async {
+        handler(update, telegram_token)
+    })
+    .await;
 }
 
-impl App {
-    fn new(tele_token: String, openai_key: String, msg: Message) -> Self {
-        let tele = Telegram::new(tele_token.clone());
-        Self {
-            tele,
-            tele_token,
-            openai_key,
-            msg,
-        }
+fn handler(update: Update, tele_token: String) {
+    if let UpdateKind::Message(msg) = update.kind {
+        let tele = Telegram::new(tele_token);
+
+        let id = get_image_id(&msg).unwrap_or("...".to_string());
+        let text = msg.text().unwrap_or("...");
+
+        let re_msg = format!("text: {text}\nimage: {id}");
+
+        _ = tele.send_message(msg.chat.id, re_msg);
     }
 }
 
-#[no_mangle]
-pub fn run() {
-    let telegram_token = env::var("telegram_token").unwrap();
-    let openai_key_name = env::var("openai_key_name").unwrap_or("jaykchen".to_string());
-
-    listen_to_update(telegram_token.clone(), |update| {
-        handler(update, telegram_token, openai_key_name)
-    });
-}
-
-fn handler(update: Update, tele_token: String, openai_key: String) {
-    if let UpdateKind::Message(msg) = update.kind {
-        let app = App::new(tele_token, openai_key, msg);
-
-        let state = app.state().unwrap_or(State::Normal);
-
-        match state {
-            State::Normal => app.normal_stuff(),
-            State::Chat => app.chat_stuff(),
-        }
+fn get_image_id(msg: &Message) -> Option<String> {
+    match (msg.document(), msg.photo().map(|p| p.last())) {
+        (Some(doc), None) => Some(doc.file.id.clone()),
+        (None, Some(Some(ps))) => Some(ps.file.id.clone()),
+        (_, _) => None,
     }
 }
